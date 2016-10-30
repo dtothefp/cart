c332a4ecc5a8317dfea996d58ea2f6a7e6bf765d
dtothefp <dtothefp@gmail.com>
2016-10-30 17:23:20 -0400

    Deploy to GitHub Pages: 4a007c9fe2bc6899c37b1456b197873449e3598b

--- deploy/bin/deploy.sh
+++ deploy/bin/deploy.sh
@@ -41,19 +41,19 @@ doCompile
 # fi
 
 # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
-ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
-chmod 600 deploy_key
-eval `ssh-agent -s`
-ssh-add deploy_key
+# ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+# ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+# ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+# ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+# openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
+# chmod 600 deploy_key
+# eval `ssh-agent -s`
+# ssh-add deploy_key
 
 # Now let's go have some fun with the cloned repo
 cd $BUILD_TARGET
-git config user.name "Travis CI"
-git config user.email "$COMMIT_AUTHOR_EMAIL"
+# git config user.name "Travis CI"
+# git config user.email "$COMMIT_AUTHOR_EMAIL"
 
 # If there are no changes to the compiled out (e.g. this is a README update) then just bail.
 # if [ -z `git diff --exit-code` ]; then
